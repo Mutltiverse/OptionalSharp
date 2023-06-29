ae49bea1e70e0270e67121040b7c81c11fc5c30c
Fox <t1758219@gmail.com>
2023-06-29 15:51:08 +0100

    optional pattern -> OptionalSharp, added Extension methods.

---
+++ ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class OptionalExtensions
+    {
+        public static Optional<T> ToOptional<T>(this T value)
+        {
+            return value != null ? Optional<T>.Of(value) : Optional<T>.None;
+        }
+    }
+}
