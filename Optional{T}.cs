8cacd36ba38b22355bd3362357216906ef656edb
Fox <t1758219@gmail.com>
2023-06-29 15:24:45 +0100

    Encapsulated constructor for better control and improved code clarity

--- Optional{T}.cs
+++ Optional{T}.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System;
 
 namespace Utility
 {
-    /// <summary>
-    /// Optional pattern exists to make it more obvious and avoid nullable contexts and confusion.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
     public class Optional<T> : IEquatable<Optional<T>>
     {
         public T Value { get; }
         public bool HasValue { get; }
 
-        public Optional(T value)
+        private Optional(T value)
         {
             Value = value;
             HasValue = true;
         }
 
-        public Optional() { }
+        private Optional() { }
 
+        public static Optional<T> Of(T value) => new Optional<T>(value);
 
         public static Optional<T> None => new Optional<T>();
 
@@ -40,7 +34,6 @@ namespace Utility
             return EqualityComparer<T>.Default.Equals(Value, other.Value);
         }
 
-        // Ensure the base class Equals is overridden as well.
         public override bool Equals(object obj)
         {
             return obj is Optional<T> optional && Equals(optional);
@@ -50,11 +43,7 @@ namespace Utility
         {
             // Hash code should be calculated based on HasValue and Value.
             // If HasValue is false, Value is ignored.
-            var hashCode = -1827433384;
-            hashCode = hashCode * -1521134295 + HasValue.GetHashCode();
-            hashCode = hashCode * -1521134295 + EqualityComparer<T>.Default.GetHashCode(Value);
-            return hashCode;
+            return HasValue ? Value.GetHashCode() : 0;
         }
-
     }
 }
