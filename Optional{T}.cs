5c00c5312c413e99c4c122ec0ec8f0a40b32d30e
Fox <t1758219@gmail.com>
2023-06-29 17:15:24 +0100

    Comments and changed to struct.

--- Optional{T}.cs
+++ Optional{T}.cs
@@ -3,22 +3,50 @@ using System;
 
 namespace Utility
 {
-    public class Optional<T> : IEquatable<Optional<T>>
+    public struct Optional<T> : IEquatable<Optional<T>>
     {
+        /// <summary> Potentially thhe value of the optional instance. </summary>
         public T Value { get; }
+
+        /// <summary> Indicating whether the Optional instance has a value. </summary>
         public bool HasValue { get; }
 
+
+        /// <summary> 
+        /// Initializes a new instance of the Optional class with a value. 
+        /// 
+        /// <para><strong>Design Note:</strong> Private so forced to use the <see cref="Optional{T}.Of(T)"/> mechanism.</para>
+        /// </summary>
         private Optional(T value)
         {
             Value = value;
             HasValue = true;
         }
 
-        private Optional() { }
-
+        /// <summary>
+        /// Creates a new instance of the Optional class with a value.
+        /// </summary>
+        /// <param name="value">The value to use.</param>
         public static Optional<T> Of(T value) => new Optional<T>(value);
 
+        /// <summary> Gets an Optional instance with no value. </summary>
         public static Optional<T> None => new Optional<T>();
+
+        /// <summary>
+        /// Returns the optional value if it exists, otherwise returns the default value.
+        /// 
+        /// <para>Example:</para> 
+        /// <code>
+        /// Optional&lt;int&gt; optionalWithValue = Optional&lt;int&gt;.Of(42);
+        /// Console.WriteLine(optionalWithValue.ValueOrElse(0));  // Output: 42
+        ///
+        /// Optional&lt;int&gt; optionalWithoutValue = Optional&lt;int&gt;.None;
+        /// Console.WriteLine(optionalWithoutValue.ValueOrElse(0));  // Output: 0
+        /// </code>
+        /// </summary>
+        /// <param name="defaultValue">The value to return if the optional does not have a value.</param>
+        /// <returns>The optional value if it exists, otherwise the default value.</returns>
+        public T ValueOrElse(T defaultValue) => HasValue ? Value : defaultValue;
 
         public bool Equals(Optional<T> other)
         {
@@ -34,10 +62,7 @@ namespace Utility
             return EqualityComparer<T>.Default.Equals(Value, other.Value);
         }
 
-        public override bool Equals(object obj)
-        {
-            return obj is Optional<T> optional && Equals(optional);
-        }
+        public override bool Equals(object obj) => obj is Optional<T> optional && Equals(optional);
 
         public override int GetHashCode()
         {
