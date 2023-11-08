7a60c3e1453cafe2ca22d90320aac4c020695a7f
Fox <t1758219@gmail.com>
2023-11-08 21:45:43 +0000

    ShortCircuitForResult extension method.

---
+++ OptionalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+using Utility;
+
+public static class OptionalExtensions
+{
+    public static Optional<TResult> ShortCircuitForResult<T, TResult>(this IEnumerable<T> source, Func<T, Optional<TResult>> function)
+    {
+        foreach (T element in source)
+        {
+            var result = function(element);
+            if (result.HasValue)
+            {
+                return result;
+            }
+        }
+
+        return Optional<TResult>.None;
+    }
+}
